7793dcc4181d88a7dfab2dc98372de97ec01227a
Ihor Porokhnenko <ihor.porokhnenko@gmail.com>
2021-11-26 16:58:00 +0200

    add files to project. first commit

---
+++ web/app/Api/V1/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @var Laravel\Lumen\Routing\Router $router
+ */
+$router->group([
+    'prefix' => env('APP_API_VERSION', ''),
+    'namespace' => '\App\Api\V1\Controllers',
+    'middleware' => 'checkUser'
+], function ($router) {
+    /**
+     * Transactions
+     */
+    $router->group([
+        'prefix' => 'transactions',
+    ], function ($router) {
+        $router->get('/', 'TransactionsController');
+    });
+
+    /**
+     * ADMIN PANEL
+     */
+    $router->group([
+        'prefix' => 'admin',
+        'namespace' => 'Admin',
+        'middleware' => 'checkAdmin'
+    ], function ($router) {
+    });
+});
