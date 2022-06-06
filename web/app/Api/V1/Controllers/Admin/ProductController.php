0717f1c26d2f47e8417a96a55dc8dc519c076272
Taiwo Soewu <taiwo_soewu.1995@ymail.com>
2022-06-06 10:59:20 +0100

    read all products

--- web/app/Api/V1/Controllers/Admin/ProductController.php
+++ web/app/Api/V1/Controllers/Admin/ProductController.php
@@ -6,9 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Exception;
 
 class ProductController extends Controller
 {
+    /**
+     * @param Product $model
+     */
+    private Product $model;
+
+    public function __construct(Product $model)
+    {
+        $this->model = $model;
+    }
+
     /**
      * Getting product detail by platform
      *
@@ -34,17 +45,98 @@ class ProductController extends Controller
      *         }
      *     },
      *
+     *     @OA\Parameter(
+     *         name="limit",
+     *         in="query",
+     *         description="Limit",
+     *         @OA\Schema(
+     *             type="number"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="page",
+     *         in="query",
+     *         description="Count",
+     *         @OA\Schema(
+     *             type="number"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="search",
+     *         in="query",
+     *         description="Search keywords",
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="sort-by",
+     *         in="query",
+     *         description="Sort by field ()",
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="sort-order",
+     *         in="query",
+     *         description="Sort order (asc, desc)",
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *
      *     @OA\Response(
-     *          response="200",
-     *          description="Getting product detail by platform"
+     *         response="200",
+     *         description="Success send data"
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="Invalid request"
+     *     ),
+     *     @OA\Response(
+     *         response="403",
+     *         description="Forbidden"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Not found"
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal server error"
      *     )
      * )
      *
      * @return mixed
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        try {
+            // Get products
+            $products = $this->model
+            ->orderBy($request->get('sort-by', 'created_at'), $request->get('sort-order', 'desc'))
+            ->paginate($request->get('limit', 20));
+
+            // Return response
+            return response()->jsonApi([
+                'type' => 'success',
+                'title' => "Products list",
+                'message' => 'List of products successfully received',
+                'data' => $products->toArray()
+            ], 200);
+        } catch (Exception $e) {
+            return response()->jsonApi([
+                'type' => 'danger',
+                'title' => "Products list",
+                'message' => $e->getMessage(),
+                'data' => null
+            ], 400);
+        }
     }
 
     /**
