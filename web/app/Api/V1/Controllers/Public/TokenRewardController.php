d0a763aa469d2547220ff073edbe3be773964f3b
Taiwo Soewu <taiwo_soewu.1995@ymail.com>
2022-07-02 13:30:43 +0100

    rewrote swagger descriptions

--- web/app/Api/V1/Controllers/Public/TokenRewardController.php
+++ web/app/Api/V1/Controllers/Public/TokenRewardController.php
@@ -37,7 +37,31 @@ class TokenRewardController extends Controller
      *     ),
      *     @OA\Response(
      *         response="200",
-     *         description="Success",
+     *         description="Successfully save"
+     *     ),
+     *     @OA\Response(
+     *         response="201",
+     *         description="Purchase created"
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="Invalid request"
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="not found"
+     *     ),
+     *     @OA\Response(
+     *         response="422",
+     *         description="Validation failed"
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Unknown error"
      *     )
      * )
      *
