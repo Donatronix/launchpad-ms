105836752727281c98643a8210f303a72e893e57
Dhana <ihor.porokhnenko@gmail.com>
2022-08-06 16:17:47 +0300

    move numerator to SDK

--- web/app/Models/Order.php
+++ web/app/Models/Order.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Traits\NumeratorTrait;
+use Sumra\SDK\Traits\NumeratorTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
