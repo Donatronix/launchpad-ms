3d5aa250634d9ab1e490bff7f9f0c4abeef7b3ca
Sikiru Shittu <afreekamode@mail.com>
2022-08-12 12:00:46 +0100

    Fix: Notification listener logic

--- web/app/Listeners/PaymentUpdate/SendNotificationListener.php
+++ web/app/Listeners/PaymentUpdate/SendNotificationListener.php
@@ -5,18 +5,18 @@ namespace App\Listeners\PaymentUpdate;
 use Sumra\SDK\Facades\PubSub;
 
 /**
- * Class GmetListenerRequest
+ * Class SendNotificationListener
  *
  * @package App\Listeners
  */
-class GmetListenerRequest
+class SendNotificationListener
 {
     /**
      * @param $document
      */
     public static function exec($document){
         // Send message to notificationMS
-        PubSub::publish('GmetListenerRequest', [
+        PubSub::publish('SendNotificationListener', [
             'type' => class_basename(get_class($document)),
             'user_id' => $document->user_id,
             'service' => env('RABBITMQ_EXCHANGE_NAME'),
