e0d5a57cd5afd25c23d3eea670644d8e71d3804d
Sikiru Shittu <afreekamode@mail.com>
2022-08-12 11:55:42 +0100

    Fix: Notification listener logic

--- web/app/Listeners/PaymentUpdate/GmetListenerRequest.php
+++ web/app/Listeners/PaymentUpdate/GmetListenerRequest.php
@@ -16,18 +16,11 @@ class GmetListenerRequest
      */
     public static function exec($document){
         // Send message to notificationMS
-        $message = [
-            'amount' => $document->amount,
-            'amount_token' => $document->total_token,
-            'document_id' => $document->id,
-            'currency' => $document->currency_code,
-            'document_object' => class_basename(get_class($document)),
-        ];
         PubSub::publish('GmetListenerRequest', [
             'type' => class_basename(get_class($document)),
             'user_id' => $document->user_id,
-            'service' => env('APP_PLATFORM'),
-            'message' => $message,
+            'service' => env('RABBITMQ_EXCHANGE_NAME'),
+            'message' => $document,
         ], config('pubsub.queue.notifications'));
     }
 }
