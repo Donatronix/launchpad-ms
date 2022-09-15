4e21eaaefaf8d142559f4e29fcfdd6b33912422c
Donald Blessing <donaldblessing9@gmail.com>
2022-09-15 08:37:33 +0100

    Minor refactor

--- web/app/Http/Resources/ProductResource.php
+++ web/app/Http/Resources/ProductResource.php
@@ -5,7 +5,7 @@
     use Illuminate\Http\Request;
     use Illuminate\Http\Resources\Json\JsonResource;
 
-    class ProductResource extends JsonResource
+    class thisResource extends JsonResource
     {
         /**
          * Transform the resource into an array.
@@ -17,18 +17,18 @@
         public function toArray($request)
         {
             return [
-                'id' => $product->id,
-                'title' => $product->title,
-                'ticker' => $product->ticker,
-                'supply' => $product->supply,
-                'sold' => $product->sold,
-                'presale_percentage' => $product->presale_percentage,
-                'icon' => $product->icon,
-                'start_date' => $product->start_date,
-                'end_date' => $product->end_date,
+                'id' => $this->id,
+                'title' => $this->title,
+                'ticker' => $this->ticker,
+                'supply' => $this->supply,
+                'sold' => $this->sold,
+                'presale_percentage' => $this->presale_percentage,
+                'icon' => $this->icon,
+                'start_date' => $this->start_date,
+                'end_date' => $this->end_date,
                 'status',
-                'created_at' => $product->created_at,
-                'updated_at' => $product->updated_at,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
             ];
         }
     }
